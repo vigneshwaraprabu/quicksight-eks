ca3e6ed6d336ef2aed66d0bd045f9585917b979c
Vigneshwaraprabu S <vigneshwaraprabus@presidio.com>
2026-02-12 22:05:18 +0530

    updated the AMI_Age in integer

--- moduler/modules/cluster_analyzer.py
+++ moduler/modules/cluster_analyzer.py
@@ -104,7 +104,7 @@ class ClusterAnalyzer:
                 "InstanceID": instance_id,
                 "Current_AMI_ID": node.get("Current_AMI_ID", "N/A"),
                 "Current_AMI_Publication_Date": node.get("Current_AMI_Publication_Date", "N/A"),
-                "AMI_Age": node.get("AMI_Age", "N/A"),
+                "AMI_Age(in days)": node.get("AMI_Age", "N/A"),
                 "OS_Version": os_version,
                 "InstanceType": node.get("InstanceType", "N/A"),
                 "NodeState": node.get("NodeState", "N/A"),
@@ -129,7 +129,7 @@ class ClusterAnalyzer:
             "ClusterVersion": cluster_version
         }
         empty_fields = dict.fromkeys([
-            "InstanceID", "Current_AMI_ID", "Current_AMI_Publication_Date", "AMI_Age", 
+            "InstanceID", "Current_AMI_ID", "Current_AMI_Publication_Date", "AMI_Age(in days)", 
             "OS_Version", "InstanceType", "NodeState", "NodeUptime", 
             "Latest_AMI_ID", "New_AMI_Publication_Date", "PatchPendingStatus",
             "NodeReadinessStatus"
